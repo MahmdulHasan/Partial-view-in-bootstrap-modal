432799068f7c6750e157b7c3227ff6309a1d8231
Mahmudul Hasan <mahmudul92hasan@gmail.com>
2019-01-30 16:30:05 +0600

    Bootstrap modal using partial view

---
+++ Controllers/ListInBootstrapModalController.cs
@@ -0,0 +1,31 @@
+﻿using PopUp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PopUp.Controllers
+{
+    public class ListInBootstrapModalController : Controller
+    {
+        private ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: ListInBootstrapModal
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            List<Employee> employeeList = _db.Employees.ToList<Employee>();
+            return Json(new { data = employeeList }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult Details(int id)
+        {
+            List<Employee> employeeList = _db.Employees.Where(w=>w.EmployeeId==id).ToList();
+            return PartialView("_Details", employeeList);
+        }
+    }
+}
